e1484a90dd3a2b3017504ff6a61738c0d651c235
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-03-30 18:00:28 -0300

    FEAT: add the logic inside the layout for load the tanstack

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "./globals.css";
+import { QueryProvider } from "../providers/QueryProvider";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  
+
+  return (
+    <html lang="en">
+      <body>
+        <QueryProvider>
+          {children}
+        </QueryProvider>
+      </body>
+    </html>
+  );
+}
