c3b3cbdb0500b815b092e945448c80fa4532d326
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-03-30 18:11:38 -0300

    FEAT: get the api weather data

---
+++ src/features/weather/services/weatherApi.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { WeatherData, weatherParams, WeatherResponse } from "../types";
+
+
+const url = "https://api.open-meteo.com/v1/forecast";
+
+export async function getWeather(latitude: number, longitude: number): Promise<WeatherResponse | void> {
+    try {
+
+        const Params : weatherParams = {
+            latitude: latitude,
+            longitude: longitude,
+            daily: ["temperature_2m_max", "temperature_2m_min", "sunrise", "sunset", "uv_index_max", "precipitation_probability_max"],
+            hourly: "temperature_2m",
+            current: ["temperature_2m", "relative_humidity_2m", "apparent_temperature", "wind_speed_10m", "cloud_cover"],
+        };
+        
+        const response = await axios.get<WeatherData>(url, { params: Params, timeout: 10000 });
+        
+        return {
+            status: response.status,
+            weatherData: response.data
+        };
+
+    } catch (error: unknown) {
+        if(axios.isAxiosError(error)) {
+            return {
+                status: error.response?.status || 500,
+                weatherData: undefined
+            };
+        } else {
+            return {
+                status: 500,
+                weatherData: undefined
+            };
+        }        
+    }
+}
