242e86b073bc88dda4875c180ded9c1f1fd4174c
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-03-30 18:07:44 -0300

    FEAT: get the api data with axios

---
+++ src/features/location/services/LocationApi.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { GeocodingData, GeocodingResponse } from "../types";
+
+export async function getCityGeocodingData(city: string): Promise<GeocodingResponse> {
+    try {
+        const geocodingUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=5&language=en&format=json`;
+        const response = await axios.get<GeocodingData>(geocodingUrl, {timeout: 10000});
+       
+        return {
+            status: response.status,
+            geocodingData: response.data
+        };
+
+        
+    } catch (error: unknown) {
+        console.error("Error fetching geocoding data:", error);
+        if(axios.isAxiosError(error)) {
+            return {
+                status: error.response?.status || 500,
+                geocodingData: undefined
+            };
+        } else {
+            return {
+                status: 500,
+                geocodingData: undefined
+            };
+        }        
+    }
+}
