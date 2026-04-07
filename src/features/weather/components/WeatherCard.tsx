c20b3d70eb8411d39bf9d6e0776aa80f49927aa1
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-04-07 19:29:10 -0300

    FEAT: update the getIcon params and fix the grid for sm screens

--- src/features/weather/components/WeatherCard.tsx
+++ src/features/weather/components/WeatherCard.tsx
@@ -1,22 +1,35 @@
 "use client"
 
 import { useWeatherQuery } from "../hooks/useWeatherQuery";
+import { GetWeatherIcon } from "../hooks/useWeatherIcon";
+import { useLocalTime } from "../hooks/useLocalTime";
 
-export default function WeatherCard(){
-    const { data, isLoading, error} = useWeatherQuery(); 
+export default function WeatherCard() {
+    const { GetIcon } = GetWeatherIcon();
+    const { data, isLoading, error } = useWeatherQuery();
+    const { localTime, isDaytime } = useLocalTime();
+    const isDay = localTime ? isDaytime(localTime) : false;
 
-    return(
+    return (
         <div className="h-[40vh] w-full flex justify-center items-center flex-col">
-            <h1>Curr weather</h1>
             {isLoading && <p>Loading weather data...</p>}
             {error && <p>Error occurred while fetching weather data.</p>}
-            {data &&
-                <div>
-                    <p>Temperature: {data.current.apparent_temperature}°C</p>
-                    <p>Umidy: {data.current.relative_humidity_2m}%</p>
-                </div>
-            }
-           
+            {data && (() => {
+
+                const Icon = GetIcon(data.current.cloud_cover,isDay);
+
+                return (
+                    <div className="grid grid-cols-2 h-[40vh]">
+                        <Icon className="col-span-1 h-full w-30 flex items-right justify-center" />
+                        <div className="col-span-1 flex flex-col items-left justify-center">
+                            <p>Temperature: {data.current.apparent_temperature}°C</p>
+                            <p>Humidity: {data.current.relative_humidity_2m}%</p>
+                            <p>Cloud cover: {data.current.cloud_cover}%</p>
+                        </div>
+                    </div>
+                );
+            })()}
+
         </div>
     )
 }
