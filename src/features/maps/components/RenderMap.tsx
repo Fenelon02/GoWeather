d6ca176d1206824a4e1fcfe3735a095ea9c215d1
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-04-13 16:20:23 -0300

    CHORE: Fix the isDaytimestate for recife the new isdaytime params

--- src/features/maps/components/RenderMap.tsx
+++ src/features/maps/components/RenderMap.tsx
@@ -6,7 +6,7 @@ import { useMaps } from "../hooks/useMaps";
 export function RenderMap() {
     useMaps();
     const {isDaytime, localTime} = useLocalTime()
-    const isDaytimeState = localTime ? isDaytime(localTime.slice(10,19)) : false;
+    const isDaytimeState = localTime ? isDaytime(localTime) : false;
 
     return (
         <div className="bg-white/20 rounded-lg p-2 my-4 min-w-full">
