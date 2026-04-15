0eaa9822cea755dac8a88aa9311ca62f1b2c6718
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-04-15 15:39:42 -0300

    FEAT: add the render curr weather data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { SearchLocationBox } from "../features/location/components/SearchLocatio
 import { useLocalTime } from "../features/weather/hooks/useLocalTime";
 import { RenderTemperatureByHour } from "../features/weather/components/RenderTemperatureByHour";
 import { RenderWeeklyTemperature } from "../features/weather/components/RenderWeeklyTemperature";
+import { RenderCurrWeatherData } from "../features/weather/components/RenderCurrWeatherData";
 
 export default function Home() {
   const { localTime, isDaytime } = useLocalTime();
@@ -15,9 +16,10 @@ export default function Home() {
     <div className={`px-15 flex flex-col items-center justify-center 
       ${isDaytimeState ? 'bg-linear-to-b from-sky-300 to-yellow-200 text-black'
         : 'bg-linear-to-b from-blue-950 to-blue-700 text-white'} 
-      min-h-screen max-w-screen py-15`}>
+      min-h-screen max-w-screen py-15 gap-4`}>
       <WeatherCard />
       <RenderTemperatureByHour />
+      <RenderCurrWeatherData/>
       <RenderMap />
       <RenderWeeklyTemperature />
       <SearchLocationBox />
