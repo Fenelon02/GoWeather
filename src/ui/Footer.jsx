6648de49a2e42aa5aaf52d9c27729e336c094340
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-04-17 18:49:17 -0300

    FEAT: create the footer (NOT FINISHED)

---
+++ src/ui/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+        <footer className="w-full h-16 flex items-center justify-center text-sm text-gray-400">
+            <p>Made by João Fenelon</p>
+        </footer>
+    )
+}
