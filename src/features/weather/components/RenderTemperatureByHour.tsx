82ec72c67e57e41244633eda44da8968820107cb
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-04-17 18:47:51 -0300

    FEAT: Adds responsiveness with a focus on text, and creates a skeleton for the element."

--- src/features/weather/components/RenderTemperatureByHour.tsx
+++ src/features/weather/components/RenderTemperatureByHour.tsx
@@ -3,6 +3,7 @@ import { useMemo } from "react";
 import { useWeatherQuery } from "../hooks/useWeatherQuery";
 import { useLocalTime } from "../hooks/useLocalTime";
 import { GetWeatherIcon } from "../hooks/useWeatherIcon";
+import Skeleton  from "react-loading-skeleton";
 
 export function RenderTemperatureByHour() {
     const {data, isLoading, error} = useWeatherQuery();
@@ -23,14 +24,19 @@ export function RenderTemperatureByHour() {
     }, [data, localTime]);
 
     return (
-        <div className="flex overflow-y-scroll w-[90vw] max-h-[25vh] bg-white/20 p-4 rounded-lg">
+        <div className="flex overflow-x-scroll w-[90vw] max-h-[25vh] bg-white/20 p-4 rounded-lg">
             <div className="flex">
                 {hourlyTemp.length > 0 && hourlyTemp.map((temp, index) => {
                     
                     const Icon = GetIcon(temp.precipitation_probability, (isDaytime(temp.date)));
 
                     return (
-                        <div key={index} className="py-1 mx-2 justify-center items-center flex flex-col gap-2">
+                        <div key={index} className="
+                        py-1 mx-2 justify-center 
+                        items-center flex flex-col 
+                        gap-2
+                        md:text-xl
+                        ">
                             {temp.date.slice(11,16)}
                             <Icon/> 
                             {temp.temperature}°C
@@ -38,7 +44,17 @@ export function RenderTemperatureByHour() {
                     )
                 })}
             </div>
-            {isLoading && <div className="flex justify-center items-center h-full">Loading hourly temperature data...</div>}
+
+            {isLoading && <div className="flex justify-center items-center h-full">
+                {Array.from({ length: 6 }).map((_, index) => (
+                    <div key={index} className="flex flex-col items-center mx-2">
+                        <Skeleton width={40} height={20} />
+                        <Skeleton width={30} height={30} circle />
+                        <Skeleton width={40} height={20} />
+                    </div>
+                ))}
+            </div>}
+
             {error && <div className="flex justify-center items-center h-full">Error occurred while fetching hourly temperature data.</div>}
         </div>
     )
