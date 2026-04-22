f6d8acea72841f187bf90591635353c7368e7416
Fenelon02 <joaofenelonramossoares@gmail.com>
2026-04-22 15:52:06 -0300

    FEAT: Adds style to fill the entire grid space in LG Screens on the homepage.

--- src/features/maps/components/RenderMap.tsx
+++ src/features/maps/components/RenderMap.tsx
@@ -9,12 +9,23 @@ export function RenderMap() {
     const isDaytimeState = localTime ? isDaytime(localTime) : false;
 
     return (
-        <div className="bg-white/20 rounded-lg p-2 w-[90vw]">
+        <div className="
+        bg-white/20 rounded-lg p-2 w-[90vw] 
+        lg:w-full
+        lg:h-full    
+        lg:flex
+        lg:flex-col
+        ">
             <h2 className={`
                 text-center ${isDaytimeState ? "  text-black" 
                 : " text-white"} py-1 md:text-xl`}
             >Cloud Coverage</h2>
-            <div id="map" className="max-w-[90vw] min-h-[40vh]"/>
+            <div id="map" className="
+            max-w-[90vw] min-h-[40vh]
+            lg:flex-1
+            lg:min-h-0
+            lg:w-full   
+            "/>
         </div>
     );
 }
